f7d9ed7c78ac280fb3a7f4d9d9676498ef634841
dhanaji33 <dhanajiwaghmode33@gmail.com>
2022-06-02 13:23:46 +0530

    HTML css basic template responsive

---
+++ HTML_CSS_Training_Template/script.js
@@ -0,0 +1,6 @@
+var menu = document.querySelector("#menu");
+var navSection = document.querySelector("#nav-section");
+function showmenu() {
+  console.log("clicked");
+}
+menu.addEventListener("click", showmenu);
